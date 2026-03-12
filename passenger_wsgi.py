2c07cebecbd239bbd5c9c3cdab9f568076b1e586
Rakshit Rangarajan <61931725+Rakshit-Rangarajan@users.noreply.github.com>
2026-03-12 23:31:08 +0530

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,4 +1,6 @@
+import os
+import sys
+sys.path.insert(0, os.path.dirname(__file__))
 from a2wsgi import ASGIMiddleware
-from app.main import app  # Import your FastAPI app.
-
+from app.main import app
 application = ASGIMiddleware(app)
